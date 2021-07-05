ea483718a94e5af68bdb93c5ac985c6f6fd5ab0b
DaCurse <dacurse0@gmail.com>
2021-07-05 20:22:45 +0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
