1f1d7ee68376cdf8d2896c8fda6d53d6819ac11f
DaCurse <dacurse0@gmail.com>
2021-07-06 00:57:49 +0300

    Change naming of links feature

--- packages/backend/src/links/links.module.ts
+++ packages/backend/src/links/links.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { LinkRepository } from './link.repository';
+import { LinksRepository } from './links.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LinkRepository])],
+  imports: [TypeOrmModule.forFeature([LinksRepository])],
   controllers: [],
   providers: [],
 })
