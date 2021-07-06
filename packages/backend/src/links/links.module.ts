55c7103dec47ba5447a00ea930c483300c590133
DaCurse <dacurse0@gmail.com>
2021-07-06 21:56:35 +0300

    Fix TypeORM import for LinksModule

--- packages/backend/src/links/links.module.ts
+++ packages/backend/src/links/links.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Link } from './link.entity';
 import { LinksController } from './links.controller';
-import { LinksRepository } from './links.repository';
 import { LinksService } from './links.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LinksRepository])],
+  imports: [TypeOrmModule.forFeature([Link])],
   controllers: [LinksController],
   providers: [LinksService],
 })
-export class LinkModule {}
+export class LinksModule {}
