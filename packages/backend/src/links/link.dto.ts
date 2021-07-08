eb0917ce7489653f5d4871aa2f73ee822e651741
DaCurse <dacurse0@gmail.com>
2021-07-08 20:03:23 +0300

    Added validation to LinkDTO

--- packages/backend/src/links/link.dto.ts
+++ packages/backend/src/links/link.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsOptional, IsUrl } from 'class-validator';
+
 export class LinkDTO {
+  @IsNotEmpty()
+  @IsUrl()
   url: string;
+  @IsOptional()
+  @IsNotEmpty()
   code?: string;
 }
