06ac024a3f38ad5c8579d8298718abbc03be76fb
DaCurse <dacurse0@gmail.com>
2021-07-08 21:56:35 +0300

    Imported LinksModule to AppModule

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
+import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig)],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forRoot(typeormConfig), LinksModule],
 })
 export class AppModule {}
