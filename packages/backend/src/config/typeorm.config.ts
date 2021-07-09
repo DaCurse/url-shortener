6f9e980bda81bac94c6d6198eec0c4e7bea75485
DaCurse <dacurse0@gmail.com>
2021-07-09 18:56:11 +0300

    Reduce duplicates in typeorm config

--- packages/backend/src/config/typeorm.config.ts
+++ packages/backend/src/config/typeorm.config.ts
@@ -2,35 +2,39 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import path from 'path';
 import { TypeOrmNamingStrategy } from './typeorm-naming.strategy';
 
+const baseConfig: TypeOrmModuleOptions = {
+  namingStrategy: new TypeOrmNamingStrategy(),
+  entities: [path.join(__dirname, '../**/*.entity.{js,ts}')],
+  logging: true,
+};
+
 const productionConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: process.env.DB_PATH,
-  namingStrategy: new TypeOrmNamingStrategy(),
-  logging: true,
-  entities: [path.join(__dirname, '../**/*.entity.js')],
   synchronize: false,
 };
 
 const testConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: ':memory:',
-  namingStrategy: new TypeOrmNamingStrategy(),
-  entities: [path.join(__dirname, '../**/*.entity.{js,ts}')],
+  logging: false,
   synchronize: true,
 };
 
 const defaultConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: path.join(__dirname, '../../data.sqlite'),
-  namingStrategy: new TypeOrmNamingStrategy(),
-  logging: true,
-  entities: [path.join(__dirname, '../**/*.entity.{js,ts}')],
   synchronize: true,
 };
 
-export const typeormConfig =
+const selectedConfig =
   process.env.NODE_ENV === 'production'
     ? productionConfig
     : process.env.NODE_ENV === 'test'
     ? testConfig
     : defaultConfig;
+
+export const typeormConfig = {
+  ...baseConfig,
+  ...selectedConfig,
+} as TypeOrmModuleOptions;
