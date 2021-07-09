c160896a45b9f62d2169e1f973aea95bfa068c42
DaCurse <dacurse0@gmail.com>
2021-07-09 06:04:53 +0300

    Implemented redirect method for LinkController

--- packages/backend/src/redirect/redirect.controller.ts
+++ packages/backend/src/redirect/redirect.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { LinkService } from '../link/link.service';
 
 @Controller()
-export class RedirectController {}
+export class RedirectController {
+  constructor(private readonly linkService: LinkService) {}
+
+  @Get('/:code')
+  async redirect(@Param('code') code: string, @Res() res: Response) {
+    const link = await this.linkService.getOneByCode(code);
+    this.linkService.incrementVisitCount(code);
+    res.redirect(link.url);
+  }
+}
