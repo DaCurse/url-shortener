5e4582b0ba72a517428f0f75b010ed145b3f41de
DaCurse <dacurse0@gmail.com>
2021-07-09 19:15:10 +0300

    Added CORS and Helmet

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
   await app.listen(PORT);
 }
 bootstrap();
