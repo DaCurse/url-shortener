59d24baf25e63ce4d226b698ffdc595346dcf973
DaCurse <dacurse0@gmail.com>
2021-07-09 05:21:23 +0300

    Changed typeorm config to be env dependent

--- packages/backend/src/config/typeorm.config.ts
+++ packages/backend/src/config/typeorm.config.ts
@@ -2,13 +2,34 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import path from 'path';
 import { TypeOrmNamingStrategy } from './typeorm-naming.strategy';
 
-const inProd = process.env.NODE_ENV === 'production';
-
-export const typeormConfig: TypeOrmModuleOptions = {
+const productionConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: inProd ? process.env.DB_PATH : path.join('./data.sqlite'),
+  database: process.env.DB_PATH,
   namingStrategy: new TypeOrmNamingStrategy(),
   logging: true,
   entities: [path.join(__dirname, '../**/*.entity.js')],
-  synchronize: !inProd,
+  synchronize: false,
 };
+
+const testConfig: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: ':memory:',
+  entities: [path.join(__dirname, '../**/*.entity.{js,ts}')],
+  synchronize: true,
+};
+
+const defaultConfig: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: path.join(__dirname, '../../data.sqlite'),
+  namingStrategy: new TypeOrmNamingStrategy(),
+  logging: true,
+  entities: [path.join(__dirname, '../**/*.entity.{js,ts}')],
+  synchronize: true,
+};
+
+export const typeormConfig =
+  process.env.NODE_ENV === 'production'
+    ? productionConfig
+    : process.env.NODE_ENV === 'test'
+    ? testConfig
+    : defaultConfig;
