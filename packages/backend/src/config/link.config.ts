6d3e3ff9660cc909bd16e71ea7f0d5a0f91fbb0c
DaCurse <dacurse0@gmail.com>
2021-07-09 18:57:28 +0300

    Change link config

--- packages/backend/src/config/link.config.ts
+++ packages/backend/src/config/link.config.ts
@@ -1,2 +1,6 @@
-export const CODE_LENGTH = 12;
-export const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+const UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const DIGITS = '0123456789';
+
+export const CODE_LENGTH = 6;
+export const ALPHABET = LOWERCASE_LETTERS + UPPERCASE_LETTERS + DIGITS;
