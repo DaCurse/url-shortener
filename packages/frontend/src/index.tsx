0c3d0cae892ccece55927a0510855e040b048d0f
DaCurse <dacurse0@gmail.com>
2021-09-16 05:23:48 +0300

    Added react-query

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,14 +1,19 @@
 import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './components/App';
 import CustomThemeProvider from './containers/CustomThemeProvider';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <CustomThemeProvider>
       <CssBaseline />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </CustomThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
