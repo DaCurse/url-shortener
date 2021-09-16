b68737066187d1884f46873e0a75767b6fac8ae8
DaCurse <dacurse0@gmail.com>
2021-09-16 06:10:52 +0300

    Fix test

--- packages/frontend/src/components/App.test.tsx
+++ packages/frontend/src/components/App.test.tsx
@@ -1,6 +1,13 @@
 import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 it('renders without crashing', () => {
-  render(<App />);
+  render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
 });
