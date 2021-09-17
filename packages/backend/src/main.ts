86e0e6dbaa56fbd5631660f0120dc9dc6cd312e6
DaCurse <dacurse0@gmail.com>
2021-09-17 09:33:33 +0300

    Added HTTP logging

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import { AppModule } from './app.module';
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
+  app.use(morgan('combined'));
   await app.listen(PORT);
 }
 bootstrap();
