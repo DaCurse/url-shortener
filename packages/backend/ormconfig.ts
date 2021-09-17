d2613b4a8efebe01dc27f613611f56055556c08c
DaCurse <dacurse0@gmail.com>
2021-09-17 11:21:34 +0300

    Fix TypeORM config for tests

--- packages/backend/ormconfig.ts
+++ packages/backend/ormconfig.ts
@@ -3,17 +3,29 @@ import { ConnectionOptions } from 'typeorm';
 import { TypeOrmNamingStrategy } from './src/config/typeorm-naming.strategy';
 
 const inProd = process.env.NODE_ENV === 'production';
+const inTest = process.env.NODE_ENV === 'test';
 const dbPath = inProd ? process.env.DB_PATH : join(__dirname, 'data.sqlite');
 
-const typeormConfig: ConnectionOptions = {
+const baseConfig: Partial<ConnectionOptions> = {
   type: 'sqlite',
   namingStrategy: new TypeOrmNamingStrategy(),
   entities: [join(__dirname, './**/*.entity.{js,ts}')],
   logging: true,
-  database: dbPath,
   synchronize: !inProd,
-  migrations: ['migrations/*.js'],
-  cli: { migrationsDir: 'migrations' },
 };
 
-export default typeormConfig;
+const restConfig: Partial<ConnectionOptions> = inTest
+  ? {
+      database: ':memory:',
+      logging: false,
+    }
+  : {
+      database: dbPath,
+      migrations: ['migrations/*.js'],
+      cli: { migrationsDir: 'migrations' },
+    };
+
+export default {
+  ...baseConfig,
+  ...restConfig,
+} as ConnectionOptions;
--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from '../ormconfig';
 import { LinkModule } from './link/link.module';
 import { StaticMiddleware } from './middleware/static.middleware';
 import { RedirectModule } from './redirect/redirect.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), LinkModule, RedirectModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), LinkModule, RedirectModule],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
